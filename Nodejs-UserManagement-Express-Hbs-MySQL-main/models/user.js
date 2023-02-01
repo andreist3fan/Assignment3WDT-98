7d5e5aee12026e9360c9d826a67f370875a8394c
desertyou <and18ste@gmail.com>
2023-02-01 14:16:42 +0100

    first commit

---
+++ Nodejs-UserManagement-Express-Hbs-MySQL-main/models/user.js
@@ -0,0 +1,51 @@
+const mysql = require('mysql');
+
+// Connection Pool
+let connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+});
+
+class User {
+  constructor( first_name, last_name, email, phone, comments) {
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.phone = phone;
+    this.comments = comments;
+  }
+  save(){
+    
+  }
+  // save() {
+  //   const db = getDb();
+  //   return db
+  //     .collection('BOOKS')
+  //     .insertOne(this)
+  //     .then(result => {
+  //       console.log(result);
+  //     })
+  //     .catch(err => {
+  //       console.log(err);
+  //     });
+  // }
+
+  // static fetchAll() {
+  //   const db = getDb();
+  //   return db
+  //     .collection('BOOKS')
+  //     .find()
+  //     .toArray()
+  //     .then(books => {
+  //       console.log(books);
+  //       return books;
+  //     })
+  //     .catch(err => {
+  //       console.log(err);
+  //     });
+  // }
+}
+
+module.exports = User;
