94b157c67de0447c8ef799196ae020b8d357bf30
Enachioiu Sorin <sorinenachioiu@yahoo.com>
2023-02-01 15:52:00 +0100

    done

--- Nodejs-UserManagement-Express-Hbs-MySQL-main/models/user.js
+++ Nodejs-UserManagement-Express-Hbs-MySQL-main/models/user.js
@@ -46,7 +46,26 @@ class User {
   }
 
   update(render){
-    
+    connection.query('UPDATE user SET first_name = ?, last_name = ?, email = ?, phone = ?, comments = ? WHERE id = ?', 
+  [first_name, last_name, email, phone, comments, req.params.id], 
+  (err, rows) => {
+    if (!err) {
+      // User the connection
+      connection.query('SELECT * FROM user WHERE id = ?', [req.params.id], (err, rows) => {
+        // When done with the connection, release it
+        
+        if (!err) {
+          res.render('edit-user', { rows, alert: `${first_name} has been updated.` });
+        } else {
+          console.log(err);
+        }
+        console.log('The data from user table: \n', rows);
+      });
+    } else {
+      console.log(err);
+    }
+    console.log('The data from user table: \n', rows);
+  });
   }
 
   static search(searchTerm, render){
