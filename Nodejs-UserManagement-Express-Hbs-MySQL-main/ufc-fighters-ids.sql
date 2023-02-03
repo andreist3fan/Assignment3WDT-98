421a9c7245b8da02c03183b45b2935db8447435e
desertyou <and18ste@gmail.com>
2023-02-03 14:47:39 +0100

    code worky?

---
+++ Nodejs-UserManagement-Express-Hbs-MySQL-main/ufc-fighters-ids.sql
@@ -0,0 +1,20 @@
+INSERT INTO `user` 
+(`id`, `first_name`,  `last_name`,    `email`,                 `phone`,         `comments`, `status`) VALUES
+(1, 'Amanda',      'Nunes',        'anunes@ufc.com',        '012345 678910', '',          'active'),
+(2, 'Alexander',   'Volkanovski',  'avolkanovski@ufc.com',  '012345 678910', '',          'active'),
+(3, 'Khabib',      'Nurmagomedov', 'knurmagomedov@ufc.com', '012345 678910', '',          'active'),
+(4, 'Kamaru',      'Usman',        'kusman@ufc.com',        '012345 678910', '',          'active'),
+(5, 'Israel',      'Adesanya',     'iadesanya@ufc.com',     '012345 678910', '',          'active'),
+(6, 'Henry',       'Cejudo',       'hcejudo@ufc.com',       '012345 678910', '',          'active'),
+(7, 'Valentina',   'Shevchenko',   'vshevchenko@ufc.com',   '012345 678910', '',          'active'),
+(8, 'Tyron',       'Woodley',      'twoodley@ufc.com',      '012345 678910', '',          'active'),
+(9, 'Rose',        'Namajunas ',   'rnamajunas@ufc.com',    '012345 678910', '',          'active'),
+(10, 'Tony',        'Ferguson ',    'tferguson@ufc.com',     '012345 678910', '',          'active'),
+(11, 'Jorge',       'Masvidal ',    'jmasvidal@ufc.com',     '012345 678910', '',          'active'),
+(12, 'Nate',        'Diaz ',        'ndiaz@ufc.com',         '012345 678910', '',          'active'),
+(13, 'Conor',       'McGregor ',    'cmcGregor@ufc.com',     '012345 678910', '',          'active'),
+(14, 'Cris',        'Cyborg ',      'ccyborg@ufc.com',       '012345 678910', '',          'active'),
+(15, 'Tecia',       'Torres ',      'ttorres@ufc.com',       '012345 678910', '',          'active'),
+(16, 'Ronda',       'Rousey ',      'rrousey@ufc.com',       '012345 678910', '',          'active'),
+(17, 'Holly',       'Holm ',        'hholm@ufc.com',         '012345 678910', '',          'active'),
+(18, 'Joanna',      'Jedrzejczyk ', 'jjedrzejczyk@ufc.com',  '012345 678910', '',          'active')
